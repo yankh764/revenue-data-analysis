4f9a1dc2235375c42e95ca1e84122ab53e342831
Yan Khoury <yankhoury9@gmail.com>
2025-04-13 17:38:35 +0200

    Update analysis query

--- sql/analysis.sql
+++ sql/analysis.sql
@@ -4,18 +4,18 @@ GO
 
 -- Execute analysis query
 WITH Faulty_Positions AS (
-    SELECT COUNT(*) AS positions_missing_payment_count
+    SELECT COUNT(*) AS count_positions_missing_payment
     FROM Abrechnung_Positionen AS AP
     JOIN Abrechnung_Rechnungen AS AR ON AP.ReId = AR.ReNummer
     WHERE AR.Zahlungsdatum IS NULL OR AR.ZahlungsbetragBrutto IS NULL
 ),
 Placeholder_Media AS (
-    SELECT SUM(Nettobetrag) AS placeholder_media_revenue_sum
+    SELECT SUM(Nettobetrag) AS sum_placeholder_media_revenue
     FROM Abrechnung_Positionen
     WHERE Bildnummer = 100000000
 ),
 Faulty_Invoices AS (
-    SELECT COUNT(AR.ReNummer) AS invoices_missing_positions_count
+    SELECT COUNT(*) AS count_invoices_missing_positions
     FROM Abrechnung_Rechnungen AS AR
     LEFT JOIN Abrechnung_Positionen AS AP ON AP.ReId = AR.ReNummer
     WHERE AP.id IS NULL
